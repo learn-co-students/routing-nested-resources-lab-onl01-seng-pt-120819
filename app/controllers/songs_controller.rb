34ef4c7d14bc16e7552960fadda741759fe65eda
SMakaiTakori <makait87@icloud.com>
2020-05-21 16:08:18 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,6 +12,23 @@ class SongsController < ApplicationController
     end
   end
 
+  # Alternate solution
+
+  # def index
+  #   if params[:artist_id]
+  #     @artist = Artist.find_by(id: params[:artist_id])
+  #     if @artist.nil?
+  #       redirect_to artists_path, alert: "Artist not found"
+  #     else
+  #       @songs = @artist.songs
+  #     end
+  #   else
+  #     @songs = Song.all
+  #   end
+  # end
+
+
+
   def show
     if Song.find_by(id: params[:id])
       @song = Song.find(params[:id])  
@@ -24,6 +41,20 @@ class SongsController < ApplicationController
   def new
     @song = Song.new
   end
+
+  # Alternate Solution
+
+  # def show
+  #   if params[:artist_id]
+  #     @artist = Artist.find_by(id: params[:artist_id])
+  #     @song = @artist.songs.find_by(id: params[:id])
+  #     if @song.nil?
+  #       redirect_to artist_songs_path(@artist), alert: "Song not found"
+  #     end
+  #   else
+  #     @song = Song.find(params[:id])
+  #   end
+  # end
 
   def create
     @song = Song.new(song_params)
