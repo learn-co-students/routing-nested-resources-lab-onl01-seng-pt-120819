8d7b68bde7253ba55aea4bc12f1d989b548aedd0
8ceg6 <8ceg6@comcast.net>
2020-05-22 20:23:24 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,5 @@
 module ArtistsHelper
-  def display_artist(song)
-    song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
+  def display_artist(song123)
+    song123.artist.nil? ? link_to("Add Artist", edit_song_path(song123)) : link_to(song123.artist_name, artist_path(song123.artist))
   end
 end
